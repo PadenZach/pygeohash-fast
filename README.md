35efbdbb264cff4c0f03ad9e2111d6e442d6fcee
zpaden <padenZach@gmail.com>
2022-07-29 01:12:14 -0400

    Improve speed and allow user to define number of threads to use

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A Fast geohasher for python.
 
-Created by wrapping the rust [geohash create](https://docs.rs/crate/geohash/latest) with pyo3.
+Created by wrapping the rust [geohash crate](https://docs.rs/crate/geohash/latest) with pyo3.
 
 Huge shout out to the georust community :)
 
@@ -17,8 +17,7 @@ Results for Pygeohash
 ```
 In [14]: %%timeit
     ...: [encode(lats[i], lngs[i], 8) for i in range(len(lats))]
-    ...: 
-    ...: 
+
 10.8 s ± 77.4 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
 ```
 
@@ -26,18 +25,14 @@ Results for Pygeohash-fast encode:
 ```
 In [16]: %%timeit
     ...: [fast_encode(lngs[i], lats[i], 8) for i in range(len(lats))]
-    ...: 
-    ...: 
 3.44 s ± 11.4 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
 ```
 
 However, in order to improve things in further when encoding many points we have methods for encoding many points at once.
 ```
-In [19]: %%timeit
+In [22]: %%timeit
     ...: encode_many(lngs, lats, 8)
-    ...: 
-    ...: 
-1.25 s ± 8.44 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+155 ms ± 3.58 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
 ```
 
-So for simple encoding this library is ~8.64 times faster.
+So for simple geohash encoding this library is ~69x times faster, nice.
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,24 @@ use geohash::encode;
 use geohash::Coordinate;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
+use rayon::ThreadPoolBuildError;
 use rayon::prelude::*;
+
+/// Create a rayon thread pool of num threads.
+/// Thanks to Carl M. Kadie for writing about this on his blog.
+/// Usage:
+/// create_pool(n_threads)?.install(|| {
+///     my_rayon_func()
+/// })?;
+pub fn create_pool(num_threads: usize) -> Result<rayon::ThreadPool, ThreadPoolBuildError> {
+    match rayon::ThreadPoolBuilder::new()
+       .num_threads(num_threads)
+       .build()
+    {
+       Err(e) => Err(e.into()),
+       Ok(pool) => Ok(pool),
+    }
+ }
 
 /// Encodes x and y coordinates to a geohash of len precision.
 /// 
@@ -28,28 +45,31 @@ fn encode_py(x: f64, y: f64, len: usize) -> PyResult<String> {
 ///     xs (List[float]): List of X/Lat values.
 ///     ys (List[float]): List of Y/Lng values.
 ///     len (int): Length of encoded geohashes desired.
+///     num_threads Optional[int]: number of threads to use. Defaults to # of cpus.
 /// Returns:
 ///     List[str]: List of Geohashes.
 #[pyfunction]
 #[pyo3(name = "encode_many")]
-fn encode_many_py(xs: Vec<f64>, ys: Vec<f64>, len: usize) -> PyResult<Vec<String>> {
-    match xs
-        .into_par_iter()
-        .zip_eq(ys)
-        .map(|xy| {
-            encode(
-                Coordinate {
-                    x: (xy.0),
-                    y: (xy.1),
-                },
-                len,
-            )
-        })
-        .collect::<Result<Vec<String>, _>>()
-    {
-        Ok(results) => Ok(results),
-        Err(e) => Err(PyException::new_err(e.to_string())),
-    }
+fn encode_many_py(xs: Vec<f64>, ys: Vec<f64>, len: usize, num_threads:Option<usize>) -> Vec<String> {
+    let mut results = Vec::with_capacity(xs.capacity());
+    
+    create_pool(num_threads.unwrap_or(num_cpus::get_physical())).unwrap().install(
+        || {
+            xs.into_par_iter()
+            .zip_eq(ys)
+            .map(|xy| {
+                encode(
+                    Coordinate {
+                        x: (xy.0),
+                        y: (xy.1),
+                    },
+                    len,
+                ).unwrap()
+            })
+            .collect_into_vec(&mut results);
+        }
+    );
+    return results;
 }
 /// A Python module implemented in Rust.
 #[pymodule]
