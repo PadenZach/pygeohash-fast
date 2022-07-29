a106065eed44ebce0caf22d198433dd1297835ee
Zach Paden <zach@locallogic.co>
2022-07-29 16:34:15 -0400

    Add decode method, Unittests, Cleanup repo, and add tests to cicd.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use geohash::encode;
 use geohash::Coordinate;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
-use rayon::ThreadPoolBuildError;
 use rayon::prelude::*;
+use rayon::ThreadPoolBuildError;
 
 /// Create a rayon thread pool of num threads.
 /// Thanks to Carl M. Kadie for writing about this on his blog.
@@ -13,69 +13,90 @@ use rayon::prelude::*;
 /// })?;
 pub fn create_pool(num_threads: usize) -> Result<rayon::ThreadPool, ThreadPoolBuildError> {
     match rayon::ThreadPoolBuilder::new()
-       .num_threads(num_threads)
-       .build()
+        .num_threads(num_threads)
+        .build()
     {
-       Err(e) => Err(e.into()),
-       Ok(pool) => Ok(pool),
+        Err(e) => Err(e.into()),
+        Ok(pool) => Ok(pool),
     }
- }
+}
 
 /// Encodes x and y coordinates to a geohash of len precision.
-/// 
+///
 /// Args:
-///     x (float): The X coordinate or "Latitude".
-///     y (float): The Y coordinate, or "longitude".
+///     lng (float): The X coordinate or "Latitude".
+///     lat (float): The Y coordinate, or "longitude".
 ///     len (int): The length of geohash.
 ///
 ///  Returns:
 ///     str: The geohash string.
 #[pyfunction]
 #[pyo3(name = "encode")]
-fn encode_py(x: f64, y: f64, len: usize) -> PyResult<String> {
-    match encode(Coordinate { x: x, y: y }, len) {
+fn encode_py(lng: f64, lat: f64, len: usize) -> PyResult<String> {
+    match encode(Coordinate { x: lng, y: lat }, len) {
         Ok(geohash) => Ok(geohash),
         Err(e) => Err(PyException::new_err(e.to_string())),
     }
 }
 
-/// Encodes a list of xs and ys into a list of geohashes of length len.
-/// 
+/// Encodes a list of lngs and lats into a list of geohashes of length len.
+///
 /// Args:
-///     xs (List[float]): List of X/Lat values.
-///     ys (List[float]): List of Y/Lng values.
+///     lngs (List[float]): List of X/Lat values.
+///     lats (List[float]): List of Y/Lng values.
 ///     len (int): Length of encoded geohashes desired.
 ///     num_threads Optional[int]: number of threads to use. Defaults to # of cpus.
 /// Returns:
 ///     List[str]: List of Geohashes.
 #[pyfunction]
 #[pyo3(name = "encode_many")]
-fn encode_many_py(xs: Vec<f64>, ys: Vec<f64>, len: usize, num_threads:Option<usize>) -> Vec<String> {
-    let mut results = Vec::with_capacity(xs.capacity());
-    
-    create_pool(num_threads.unwrap_or(num_cpus::get_physical())).unwrap().install(
-        || {
-            xs.into_par_iter()
-            .zip_eq(ys)
-            .map(|xy| {
-                encode(
-                    Coordinate {
-                        x: (xy.0),
-                        y: (xy.1),
-                    },
-                    len,
-                ).unwrap()
-            })
-            .collect_into_vec(&mut results);
-        }
-    );
+fn encode_many_py(
+    lngs: Vec<f64>,
+    lats: Vec<f64>,
+    len: usize,
+    num_threads: Option<usize>,
+) -> Vec<String> {
+    let mut results = Vec::with_capacity(lngs.capacity());
+
+    create_pool(num_threads.unwrap_or(num_cpus::get_physical()))
+        .unwrap()
+        .install(|| {
+            lngs.into_par_iter()
+                .zip_eq(lats)
+                .map(|xy| {
+                    encode(
+                        Coordinate {
+                            x: (xy.0),
+                            y: (xy.1),
+                        },
+                        len,
+                    )
+                    .unwrap()
+                })
+                .collect_into_vec(&mut results);
+        });
     return results;
 }
 
+/// Decodes a geohash string into a coordinates with error.
+/// 
+/// Args:
+///     encoded_geohash: String - Geohash to decode.
+/// Returns:
+///     Tuple(lng, lat, lng_err, lat_err)
+#[pyfunction]
+#[pyo3(name = "decode")]
+fn decode_py(encoded_geohash:&str) -> PyResult<(f64, f64, f64, f64)> {
+    match geohash::decode(encoded_geohash) {
+        Ok(v) => return PyResult::Ok((v.0.x,v.0.y, v.1, v.2)),
+        Err(e) => return Err(PyException::new_err(e.to_string()))
+    }
+}
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pygeohash_fast(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(encode_py, m)?)?;
     m.add_function(wrap_pyfunction!(encode_many_py, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_py, m)?)?;
     Ok(())
 }
---
+++ tests/test_pyapi.py
@@ -0,0 +1,14 @@
+from pygeohash_fast import encode, decode, encode_many
+
+class TestClass:
+    def test_encode_works(self):
+        assert encode(-72.747917, 45.207615, 5) == "f2h30"
+    
+    def test_decode_works(self):
+        assert decode("f2h30") == (-72.75146484375, 45.19775390625, 0.02197265625, 0.02197265625)
+
+    def test_encode_many_works(self):
+        lats = [47.1, 35.204]
+        lngs = [-76.6, -80.8501]
+        expected = ["f23e", "dnq8"]
+        assert encode_many(lngs, lats, 4) == expected
